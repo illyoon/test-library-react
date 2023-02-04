f58f48c2bfce00d0f8473fde56aa883919b28c59
illias <khomko.illia@gmail.com>
2023-02-05 03:02:04 +0900

    feat: init tsdx + storybook

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+
+// Delete me
+export const Thing = () => {
+  return <div className="mt-10 bg-yellow-100 p-5 rounded-md">the snozzberries taste like snozzberries</div>;
+};
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
+  plugins: [],
+}
